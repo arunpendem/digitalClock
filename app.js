1c500cf054a4f0afeb56a53bd6c3552cc8f040e1
arunpendem <arunpendem999@gmail.com>
2020-05-25 08:51:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ console.log(minutes)
 var daysInWeek=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"]
 var today=daysInWeek[day]
 if(hours<12)
-console.log("Good morning.The time is "+hours+" hours "+minutes+" minutes AM.Today is "+today)
+document.write("Good morning.The time is "+hours+" hours "+minutes+" minutes AM.Today is "+today)
 else if(hours>12&&hours%12<=4)
-console.log("Good Afternoon.The time is "+hours%12+" hours "+minutes+" minutes PM.Today is "+today)
+document.write("Good Afternoon.The time is "+hours%12+" hours "+minutes+" minutes PM.Today is "+today)
 else
-console.log("Good Evening.The time is "+hours%12+" hours "+minutes+" minutes PM.Today is "+today)
+document.write("Good Evening.The time is "+hours%12+" hours "+minutes+" minutes PM.Today is "+today)
 
